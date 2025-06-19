95f6e65da28931a8e885ad1df724b8f35f94f356
italoweb99 <italoweb99@gmail.com>
2025-06-19 17:00:09 -0300

    mudancas nas variaveis de ambiente

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
-const SweggerUI = require('swagger-ui-express');
+const SwaggerUI = require('swagger-ui-express');
 const server = express();
 const mongoose = require('mongoose');
-const SweggerDoc = require('./documentacao.json')
+const SwaggerDoc = require('./documentacao.json')
+require('dotenv').config();
 server.use(express.json());
 const cursos  = ["Node.js"];
 
@@ -11,7 +12,7 @@ const cursos  = ["Node.js"];
 //const Funcionarios = require('./models/Funcionarios')
 const produtosRoutes = require('./routes/produtosRoutes')
 
-server.use('/docs', SweggerUI.serve, SweggerUI.setup(SweggerDoc) )
+server.use('/docs', SwaggerUI.serve, SwaggerUI.setup(SwaggerDoc) )
 server.use(
     express.urlencoded({
         extended: true
@@ -19,9 +20,7 @@ server.use(
 )
 server.use('/produtos',produtosRoutes)
 
-mongoose.connect(
-    "mongodb+srv://italoweb99:ftiSAd5Nfm0tnkTz@cluster0.dhwasik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-)
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
     console.log("conectado ao mongo")
 }
@@ -29,11 +28,4 @@ mongoose.connect(
 .catch((err)=>{
     console.log(err)
 })
-function checkCurso(req,res,next){
-    if(!req.body.novo_curso){
-        return res.status(400).json({error: "Nome do curso é obrigatorio"})
-    }
-    return next();
-}
-
 server.listen(3000);
